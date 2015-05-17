84ea1bb900026bf5db5bde72a54b21c95cc293a5
Alex Paul <axp_alpaul@yahoo.com>
2015-05-17 19:40:46 -0400

    Core Data Implementation Added

--- TaskIt/TaskModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TaskModel.swift
-//  TaskIt
-//
-//  Created by Alex Paul on 5/9/15.
-//  Copyright (c) 2015 Alex Paul. All rights reserved.
-//
-
-import Foundation
---
+++ TaskModel.swift
@@ -0,0 +1,19 @@
+//
+//  TaskModel.swift
+//  TaskIt
+//
+//  Created by Alex Paul on 5/13/15.
+//  Copyright (c) 2015 Alex Paul. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class TaskModel: NSManagedObject {
+
+    @NSManaged var task: String
+    @NSManaged var subtask: String
+    @NSManaged var date: NSDate
+    @NSManaged var isCompleted: NSNumber
+
+}
